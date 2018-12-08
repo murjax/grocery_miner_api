1e94ebe64f2a476fd8513b874d19c525e51aae62
Ryan Murphy <murjax@gmail.com>
2018-12-08 11:42:23 -0500

    Custom authenticate_user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,9 @@ class HomeController < ApplicationController
   def index
     render json: { success: true }, adapter: :json
   end
+
+  def authenticate_user!(opts={})
+    opts[:scope] = :user
+    warden.authenticate!(opts) if !devise_controller? || opts.delete(:force)
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,15 @@
 class SessionsController < Devise::SessionsController
   respond_to :json
 
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    response.headers['Access-Control-Expose-Headers'] = 'Authorization'
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
+
   private
 
   def respond_with(resource, _opts = {})
