4db89e00c384077001897ad1cf65477544ca1b4b
Ryan Murphy <murjax@gmail.com>
2019-03-11 22:43:31 -0400

    Increase session life

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -36,7 +36,7 @@ Devise.setup do |config|
     jwt.revocation_requests = [
       ['DELETE', %r{^/logout$}]
     ]
-    jwt.expiration_time = 1.day.to_i
+    jwt.expiration_time = 5.days.to_i
   end
 end
 
