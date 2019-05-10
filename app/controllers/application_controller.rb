7f468e52129a926666e9dec1dd6d9fad07edfd92
Ryan Murphy <murjax@gmail.com>
2019-05-09 21:03:28 -0400

    Add jsonapi-resources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,4 @@
-class ApplicationController < ActionController::API
-  def render_resource(resource)
-    if resource.errors.empty?
-      render json: resource
-    else
-      validation_error(resource)
-    end
-  end
-
-  def validation_error(resource)
-    render json: {
-      errors: [
-        {
-          status: '400',
-          title: 'Bad Request',
-          detail: resource.errors,
-          code: '100'
-        }
-      ]
-    }, status: :bad_request
-  end
+class ApplicationController < ActionController::Base
+  include JSONAPI::ActsAsResourceController
+  protect_from_forgery with: :null_session
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,10 +7,10 @@ Rails.application.configure do
   config.cache_classes = false
 
   # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load = true
 
   # Show full error reports.
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
